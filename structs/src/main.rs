14f6976c2eff22ecf85bf223f3ceb30fa2833212
Takao Mizuno <takao.mizuno110@gmail.com>
2022-08-14 16:43:24 +0900

    added an implementation for structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -54,9 +54,46 @@
 //    width * height
 //}
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+//fn main() {
+//    let scale = 2;
+//    let rect1 = Rectangle {
+//        width: dbg!(30 * scale),
+//        height: 50,
+//    };
+//
+//    println!(
+//        "The area of the rectangle is {} square pixels",
+//        area(&rect1)
+//    );
+//    println!("rect1 is {:#?}", rect1);
+//    dbg!(&rect1);
+//}
+//
+//fn area(rectangle: &Rectangle) -> u32 {
+//    rectangle.width * rectangle.height
+//}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -66,11 +103,11 @@ fn main() {
     };
 
     println!(
-        "The area of the rectangle is {} square pixels",
-        area(&rect1)
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
     );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 }
